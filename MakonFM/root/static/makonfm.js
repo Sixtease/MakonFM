68e339468f10782aa7067e9a2ee1c08c4daf4f8d
Oldrich Kruza <sixtease+github@gmail.com>
2011-12-30 22:36:15 +0100

    Minor cleaning of JS

--- MakonFM/root/static/makonfm.js
+++ MakonFM/root/static/makonfm.js
@@ -1,5 +1,6 @@
-var MEDIA_BASE = 'http://commondatastorage.googleapis.com/karel-makon-mp3/';
-var JPLAYER_INITIALIZED = false;
+var MakonFM = {};
+
+MakonFM.MEDIA_BASE = 'http://commondatastorage.googleapis.com/karel-makon-mp3/';
 
 $('.track-menu li').click(function(evt) {
     if ($(evt.target).is('li,li>:header')) {} else return;
@@ -9,16 +10,14 @@ $('.track-menu li').click(function(evt) {
 $('.track-menu li>a').click(function(evt) {
     var fn = $(this).text();
     $('.jp-title li').text(fn);
-    set_media(MEDIA_BASE + fn);
-});
-
-function set_media(audio_fn) {
     $('#jquery_jplayer_1').jPlayer('setMedia', {
-        mp3: audio_fn
+        mp3: MakonFM.MEDIA_BASE + fn
     });
-}
+});
 
 $("#jquery_jplayer_1").jPlayer({
     swfPath: "/static",
     supplied: "mp3"
 });
+
+
