1caa1ee5d1ac1153fb0fdba8bb59142528e69f4c
Oldrich Kruza <sixtease+github@gmail.com>
2011-12-31 00:41:51 +0100

    Started implementing subtitle display; refs #1
    upd_sub gives the .cur class to a word given a timestamp

--- MakonFM/root/static/makonfm.js
+++ MakonFM/root/static/makonfm.js
@@ -20,4 +20,61 @@ $("#jquery_jplayer_1").jPlayer({
     supplied: "mp3"
 });
 
+MakonFM.upd_sub = function (ts, subs, i) {
+    if (!subs) subs = MakonFM.subs;
+    if (!i) i = 0;
+    while (subs[i++].timestamp < ts) { }
+    while (subs[--i].timestamp > ts) { }
+    var sub = subs[i];
+    $('.subtitles .Word').removeClass('cur');
+    $('.subtitles .Word[data-timestamp="'+sub.timestamp+'"]').addClass('cur');
+};
 
+$('#ts').change(function() {
+    MakonFM.upd_sub($(this).val());
+});
+
+MakonFM.subs = [
+    {
+        timestamp: 0,
+        occurrence: 'Ťuk',
+        fonet: 'tj u k',
+        wordform: 'ťuk'
+    },
+    {
+        timestamp: 1.1,
+        occurrence: 'ťuk!',
+        fonet: 'tj u k',
+        wordform: 'ťuk'
+    },
+    {
+        timestamp: 2.2,
+        occurrence: '"Takže',
+        fonet: 't a k zh e',
+        wordform: 'takže'
+    },
+    {
+        timestamp: 3.3,
+        occurrence: 'dobrý',
+        fonet: 'd o b r ii',
+        wordform: 'dobrý'
+    },
+    {
+        timestamp: 4.4,
+        occurrence: 'večer,"',
+        fonet: 'v e ch e r',
+        wordform: 'večer'
+    },
+    {
+        timestamp: 5.5,
+        occurrence: 'řekl',
+        fonet: 'r zh e k l',
+        wordform: 'řekl'
+    },
+    {
+        timestamp: 6.6,
+        occurrence: 'jelen.',
+        fonet: 'j e l e ng',
+        wordform: 'jelen'
+    }
+];
