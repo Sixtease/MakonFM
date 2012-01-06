08d0b3cb460a6594587062b84cd286a9a97887c6
Oldrich Kruza <sixtease+github@gmail.com>
2012-01-07 00:18:26 +0100

    Subtitles are now displayed linewise; refs #1

--- MakonFM/root/static/makonfm.js
+++ MakonFM/root/static/makonfm.js
@@ -8,6 +8,11 @@ var MakonFM = {
     subtitles: {}
 };
 
+$(document).ready(function() {
+    var lh = MakonFM.SUB_LINE_HEIGHT = $('.subtitles').css('lineHeight').replace(/\D+/g, '');
+    var lc = MakonFM.SUB_LINE_CNT = Math.round($('.subtitles').height() / lh);
+    MakonFM.SUB_MIDDLE_LINE = Math.floor((lc+1) / 2) - 1;
+});
 
 $('.track-menu li').click(function(evt) {
     if ($(evt.target).is('li,li>:header')) {} else return;
@@ -54,7 +59,10 @@ MakonFM._add_st_word = function(sub, where) {
     else if (where.before) $word.insertBefore(where.before);
     else throw ('no where specified to _add_st_word');
     
-    $word.after(' ');
+    if (where.after) $word.before(' ');
+    else             $word.after (' ');
+
+    return $word;
 };
 
 MakonFM.upd_sub = function (ts, subs, i) {
@@ -62,61 +70,178 @@ MakonFM.upd_sub = function (ts, subs, i) {
     var $cur_have = $st.find('.Word.cur');
     var cur_have = $cur_have.data('timestamp');
     var next_have = $cur_have.next().data('timestamp');
-    if (cur_have > ts && next_have < ts) return;
+    if (cur_have < ts && next_have > ts) return;
+    
+    var LEFT = -1;
+    var RIGHT = 1;
+    
     if (!subs) subs = MakonFM.subs;
     i = MakonFM._i_by_ts(ts, subs, i);
     MakonFM.CURRENT_INDEX = i;
     var sub = subs[i];
     
-    var first_have = $st.find('.Word:first').data('timestamp');
-    if (first_have === undefined) {
-        MakonFM._add_st_word(sub, {onhead:$st});
-        first_have = sub.timestamp;
+    var $new_cur = $st.find('.Word[data-timestamp="'+sub.timestamp+'"]');
+    if ($new_cur == $cur_have) return;
+    var $stopper_left = null;
+    var $stopper_right = null;
+    
+    if ($new_cur.length == 0) {
+        var $first_have = $st.find('.Word:first');
+        if ($first_have.length == 0) {
+            $new_cur = MakonFM._add_st_word(sub, {onhead:$st});
+        }
+        else {
+            var first_have = $first_have.data('timestamp');
+            if (sub.timestamp < first_have) {
+                $new_cur = MakonFM._add_st_word(sub, {before:$first_have});
+                $stopper_right = $first_have;
+            }
+            else {
+                var $last_have = $st.find('.Word:last');
+                var last_have = $last_have.data('timestamp');
+                if (sub.timestamp > last_have) {
+                    $new_cur = MakonFM._add_st_word(sub, {after:$last_have});
+                    $stopper_left = $last_have;
+                }
+                else throw ('No current and not less than leftmost and not more than rightmost?');
+            }
+        }
+        add_adjacent({ant: $new_cur, dir: LEFT,  stopper: $stopper_left });
+        add_adjacent({ant: $new_cur, dir: RIGHT, stopper: $stopper_right});
     }
-    var first_need = subs[Math.max(0,i-MakonFM.WORDS_PRE)].timestamp;
-    var first_have_i = MakonFM._i_by_ts(first_have, subs, i);
-    var new_first;
-    
-    // unshift words missing on start
-    while (first_need < first_have) {
-        if (--first_have_i < 0) break;
-        new_first = subs[first_have_i];
-        MakonFM._add_st_word(new_first, {onhead:$st});
-        first_have = new_first.timestamp;
-    }
-    
-    // shift words redundant on start
-    while (first_need > first_have) {
-        $st.find('.Word:first').remove();
-        new_first = $st.find('.Word:first');
-        if (new_first.length == 0) break;
-        first_have = new_first.data('timestamp');
-    }
-    
-    var last_have = $st.find('.Word:last' ).data('timestamp');
-    var last_need = subs[Math.min(i+MakonFM.WORDS_POST,subs.length-1)].timestamp;
-    var last_have_i = MakonFM._i_by_ts(last_have, subs, i);
-    var new_last;
-    
-    // push words missing on end
-    while (last_need > last_have) {
-        if (++last_have_i > subs.length-1) break;
-        new_last = subs[last_have_i];
-        MakonFM._add_st_word(new_last, {ontail:$st});
-        last_have = new_last.timestamp;
-    }
-    
-    // pop words redundant on end
-    while (last_need < last_have) {
-        $st.find('.Word:last').remove();
-        new_last = $st.find('.Word:last');
-        if (new_last.length == 0) break;
-        last_have = new_last.data('timestamp');
+    else {
+        var cur_lineno = MakonFM._lineno_of($new_cur);
+        var lines_to_scroll = cur_lineno - MakonFM.SUB_MIDDLE_LINE;
+        scroll(lines_to_scroll);
     }
     
     $st.find('.Word').removeClass('cur');
-    $st.find('.Word[data-timestamp="'+sub.timestamp+'"]').addClass('cur');
+    $new_cur.addClass('cur');
+
+    function add_adjacent(arg) {
+        var dir = arg.dir;
+        if (!dir) throw('No dir given to add_adjacent in arg', arg);
+        var $ant = arg.ant;
+        if (!$ant || !$ant.length) throw('No ant[ecedant] given to add_adjacent in arg', arg);
+        var $stopper = arg.stopper;
+        var j = i;
+        var $added = $ant;
+        if ($stopper) {
+            var stop_ts = $stopper.data('timestamp');
+        }
+        if (dir === LEFT) {
+            if ($stopper) {
+                var $br = $('<br>')
+                .insertAfter($stopper);
+            }
+            var shifted_ref_lineno = MakonFM._lineno_of($ant) + MakonFM.SUB_MIDDLE_LINE;
+            while (MakonFM._lineno_of($ant) < shifted_ref_lineno) {
+                j--;
+                if (j<0) break;
+                if (stop_ts && subs[j].timestamp == stop_ts) {
+                    $br.remove();
+                    break;
+                }
+                $added = MakonFM._add_st_word(subs[j], {before:$added});
+            }
+            if ($stopper) {
+                if ($br) {
+                    $br.prevAll().add($br).remove();
+                }
+                else {
+                    scroll(MakonFM._lineno_of($ant) - MakonFM.SUB_MIDDLE_LINE);
+                }
+            }
+        }
+        else if (dir == RIGHT) {
+            var do_add = true;
+            var got_too_much = false;
+            while (MakonFM._lineno_of($added) < MakonFM.SUB_LINE_CNT) {
+                j++;
+                if (j >= subs.length) break;
+                if (!do_add) {
+                    $added = $added.next();
+                    $stopper = $added.next();
+                    continue;
+                }
+                if (stop_ts && subs[j].timestamp == stop_ts) {
+                    do_add = false;
+                    continue;
+                }
+                $added = MakonFM._add_st_word(subs[j], {after:$added});
+                got_too_much = true;
+            }
+            if (got_too_much) $added.remove();
+            if ($stopper) {
+                $stopper.nextAll().add($stopper).remove();
+            }
+        }
+    }
+    function scroll(n) {
+        if (n == 0) return;
+        if (n < 0) return scrollup(-n);
+        if (n > 0) return scrolldown(n);
+        console.log('bad n:',n);
+    }
+    function scrollup(n) {
+        if (n === undefined) n = 1;
+        var $start = $st.find('.Word:first');
+        var i = MakonFM._i_by_ts($start.data('timestamp'), subs);
+        var $added = $start;
+        while (MakonFM._lineno_of($start) < n) {
+            i--;
+            if (i < 0) break;
+            $added = MakonFM._add_st_word(subs[i], {before:$added});
+        }
+        var $last = $st.find('.Word:last');
+        while (MakonFM._lineno_of($last) >= MakonFM.SUB_LINE_CNT) {
+            var $to_remove = $last;
+            $last = $last.prev();
+            $to_remove.remove();
+        };
+    }
+    function scrolldown(n) {
+        if (n === undefined) n = 1;
+        var $words = $st.find('.Word');
+        var lines_added = 0;
+        
+        var $added = $words.last();
+        var i = MakonFM._i_by_ts($added.data('timestamp'), subs);
+        var prev_lineno;
+        var cur_lineno;
+        cur_lineno = prev_lineno = MakonFM._lineno_of($added);
+        while (cur_lineno < MakonFM.SUB_LINE_CNT + n) {
+            i++;
+            if (i >= subs.length) break;
+            $added = MakonFM._add_st_word(subs[i], {after:$added});
+            
+            cur_lineno = MakonFM._lineno_of($added);
+            if (prev_lineno < cur_lineno) lines_added++;
+            prev_lineno = cur_lineno;
+        }
+        // last line is just one word, delete it
+        lines_added--;
+        $added.remove();
+        
+        var $to_remove = $words.first();
+        var i = 1;
+        var $w;
+        while (MakonFM._lineno_of($w=$words.eq(i++)) < lines_added) {
+            $to_remove = $to_remove.add($w);
+        }
+        $to_remove.remove();
+    }
 };
+
+
+MakonFM._lineno_of = function($word, lh) {
+    if (lh === undefined) {
+        lh = MakonFM.SUB_LINE_HEIGHT;
+    }
+    var pos = $word.position().top;
+    return Math.floor((pos+lh/2)/lh);
+};
+
 MakonFM.clear_subs = function() {
     MakonFM.subs = [];
     $('.subtitles').empty();
