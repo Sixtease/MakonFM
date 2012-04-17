07cccacb12b814bb3f0f0c1f2723b1f70a6d46b3
Oldrich Kruza <sixtease+github@gmail.com>
2012-04-17 19:07:26 +0200

    local/internetty run is now configurable
    with $c->config->{local}

---
+++ MakonFM/root/js_init.js
@@ -0,0 +1,13 @@
+
+var MAKONFM_CONFIG = {};
+[%- IF c.config.local %]
+MAKONFM_CONFIG.MEDIA_BASE    = '/static/audio/';
+MAKONFM_CONFIG.SUBTITLE_BASE = '/static/subs/';
+MAKONFM_CONFIG.SEND_SUBTITLES_URL = '/subsubmit/';
+MAKONFM_CONFIG.SUBVERSIONS_URL    = '/subversions/';
+[%- ELSE %]
+MAKONFM_CONFIG.MEDIA_BASE    = 'http://commondatastorage.googleapis.com/karel-makon-mp3/';
+MAKONFM_CONFIG.SUBTITLE_BASE = 'http://commondatastorage.googleapis.com/karel-makon-sub/';
+MAKONFM_CONFIG.SEND_SUBTITLES_URL = '[% c.config.server_base %]/subsubmit/';
+MAKONFM_CONFIG.SUBVERSIONS_URL    = '[% c.config.server_base %]/subversions/';
+[%- END -%]
--- MakonFM/root/static/makonfm.js
+++ MakonFM/root/static/makonfm.js
@@ -2,14 +2,7 @@ $.ajaxSetup({ cache: true });
 
 function MakonFM_constructor(instance_name) {
     var m = this;
-    m.MEDIA_BASE =    'http://commondatastorage.googleapis.com/karel-makon-mp3/';
-//    m.MEDIA_BASE =    '/static/audio/';
-    m.SUBTITLE_BASE = 'http://commondatastorage.googleapis.com/karel-makon-sub/';
-//    m.SUBTITLE_BASE = '/static/subs/';
-    m.SEND_SUBTITLES_URL = '/subsubmit/';
-    m.SUBVERSIONS_URL = '/subversions/';
-    m.WORDS_PRE = 10;
-    m.WORDS_POST = 10;
+    $.extend(this, MAKONFM_CONFIG);
     m.CURRENT_INDEX = 0;
     m.name = instance_name;
 
