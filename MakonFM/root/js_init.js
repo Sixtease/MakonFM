1f33376a10b9e93b5c1f6dce34747018cb38e25e
Oldrich Kruza <sixtease+github@gmail.com>
2012-04-23 23:09:45 +0200

    Replaced GSutil subs with server-residing ones

--- MakonFM/root/js_init.js
+++ MakonFM/root/js_init.js
@@ -7,7 +7,8 @@ MAKONFM_CONFIG.SEND_SUBTITLES_URL = '/subsubmit/';
 MAKONFM_CONFIG.SUBVERSIONS_URL    = '/subversions/';
 [%- ELSE %]
 MAKONFM_CONFIG.MEDIA_BASE    = 'http://commondatastorage.googleapis.com/karel-makon-mp3/';
-MAKONFM_CONFIG.SUBTITLE_BASE = 'http://commondatastorage.googleapis.com/karel-makon-sub/';
+//MAKONFM_CONFIG.SUBTITLE_BASE = 'http://commondatastorage.googleapis.com/karel-makon-sub/';
+MAKONFM_CONFIG.SUBTITLE_BASE = '[% c.config.server_base %]/static/subs/'
 MAKONFM_CONFIG.SEND_SUBTITLES_URL = '[% c.config.server_base %]/subsubmit/';
 MAKONFM_CONFIG.SUBVERSIONS_URL    = '[% c.config.server_base %]/subversions/';
 [%- END -%]
