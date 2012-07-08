4ed8d4873b29b47ed6498b77248c10bb0f8ec755
Oldrich Kruza <sixtease+github@gmail.com>
2012-07-08 23:05:16 +0200

    indented js_init.js

--- MakonFM/root/js_init.js
+++ MakonFM/root/js_init.js
@@ -1,15 +1,15 @@
 
 var MAKONFM_CONFIG = {};
 [%- IF c.config.local %]
-MAKONFM_CONFIG.MEDIA_BASE    = '/static/audio/';
-MAKONFM_CONFIG.SUBTITLE_BASE = '/static/subs/';
+MAKONFM_CONFIG.MEDIA_BASE         = '/static/audio/';
+MAKONFM_CONFIG.SUBTITLE_BASE      = '/static/subs/';
 MAKONFM_CONFIG.SEND_SUBTITLES_URL = '/subsubmit/';
 MAKONFM_CONFIG.SUBVERSIONS_URL    = '/subversions/';
 [%- ELSE %]
-MAKONFM_CONFIG.MEDIA_BASE    = 'http://commondatastorage.googleapis.com/karel-makon-mp3/';
-//MAKONFM_CONFIG.SUBTITLE_BASE = 'http://commondatastorage.googleapis.com/karel-makon-sub/';
-MAKONFM_CONFIG.SUBTITLE_BASE = '[% c.config.server_base %]/static/subs/'
+MAKONFM_CONFIG.MEDIA_BASE         = 'http://commondatastorage.googleapis.com/karel-makon-mp3/';
+//MAKONFM_CONFIG.SUBTITLE_BASE      = 'http://commondatastorage.googleapis.com/karel-makon-sub/';
+MAKONFM_CONFIG.SUBTITLE_BASE      = '[% c.config.server_base %]/static/subs/'
 MAKONFM_CONFIG.SEND_SUBTITLES_URL = '[% c.config.server_base %]/subsubmit/';
 MAKONFM_CONFIG.SUBVERSIONS_URL    = '[% c.config.server_base %]/subversions/';
 [%- END -%]
-MAKONFM_CONFIG.STATIC_BASE = '[% c.config.static_base %]';
+MAKONFM_CONFIG.STATIC_BASE        = '[% c.config.static_base %]';
