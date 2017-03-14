10152f9b8faf5ee96756b5cb441dbf6f1f56e573
Jan Oldrich Kruza <sixtease+github@gmail.com>
2017-03-14 18:37:50 +0100

    replace jquery trigger with direct callback

--- MakonFM/script/humpart.sh
+++ MakonFM/script/humpart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "jQuery(document).trigger('got_humpart.MakonFM',{\"modified\":\"`date '+%FT%T%:::z'`\",\"data\":{"; for s in "$1"/*.sub.js; do echo "\"`basename $s | sed 's/.sub.js//'`\":{\"human\":" `grep -w humanic "$s" | wc -l` ',"total":' `grep -w occurrence "$s" | wc -l` '},'; done; echo '}});'
+echo "jsonp_humpart({\"modified\":\"`date '+%FT%T%:::z'`\",\"data\":{"; for s in "$1"/*.sub.js; do echo "\"`basename $s | sed 's/.sub.js//'`\":{\"human\":" `grep -w humanic "$s" | wc -l` ',"total":' `grep -w occurrence "$s" | wc -l` '},'; done; echo '}});'
