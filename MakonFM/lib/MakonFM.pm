de290dc02009e41dc0da2dc617735dc74d71d598
Jan Oldřich Krůza <jan@sixtease.net>
2025-10-21 14:55:22 +0200

    enable session cookie for cross-site

--- MakonFM/lib/MakonFM.pm
+++ MakonFM/lib/MakonFM.pm
@@ -43,6 +43,11 @@ __PACKAGE__->config(
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header => 1, # Send X-Catalyst header
+
+    'Plugin::Session' => {
+        cookie_same_site => 'None',
+        cookie_secure => 1,
+    },
 );
 
 # Start the application
