b1986a92846ced6f65d8d67717546ee56f35ee7d
Jan Oldřich Krůza <jan@sixtease.net>
2025-12-26 20:46:38 +0100

    add search handler for knihy

--- MakonFM/lib/MakonFM/Controller/Search.pm
+++ MakonFM/lib/MakonFM/Controller/Search.pm
@@ -10,9 +10,9 @@ BEGIN { extends 'Catalyst::Controller' }
 
 sub index :Path :Args(0) {
     my ($self, $c) = @_;
-    
+
     my $param = $c->request->parameters;
-    
+
     my $query = $param->{query};
     my $from = $param->{from} || 0;
     my @order_by = (parse_order_by($param->{'order_by[]'}), '_score');
@@ -58,6 +58,58 @@ sub index :Path :Args(0) {
         from => $from,
         body => \%es_query,
     );
+
+    $c->response->content_type('text/json');
+    $c->response->header('Access-Control-Allow-Origin' => '*');
+    $c->response->body(decode_utf8(encode_json($results)));
+}
+
+sub texty :Path('texty') :Args(0) {
+    my ($self, $c) = @_;
+
+    my $param = $c->request->parameters;
+    my $query = $param->{query};
+    my $from  = $param->{from}  || 0;
+    my $size  = $param->{size};
+
+    my @order_by = (parse_order_by($param->{'order_by[]'}), '_score');
+    my @filters;
+    push @filters, { term => { collection => $param->{collection} } } if $param->{collection};
+    push @filters, { term => { book_id    => $param->{book_id} } }    if $param->{book_id};
+
+    my $match_clause = $query
+        ? { match => { body => $query } }
+        : { match_all => {} };
+
+    my %es_query = (
+        query => @filters
+            ? {
+                bool => {
+                    must   => [$match_clause],
+                    filter => \@filters,
+                },
+            }
+            : $match_clause,
+        highlight => {
+            fields  => {
+                body  => {},
+            },
+            pre_tags  => ['**'],
+            post_tags => ['**'],
+        },
+        sort => \@order_by,
+    );
+
+    my $es = Search::Elasticsearch->new;
+
+    my %search = (
+        index => 'knihy',
+        from  => $from,
+        body  => \%es_query,
+    );
+    $search{size} = $size if defined $size;
+
+    my $results = $es->search(%search);
 
     $c->response->content_type('text/json');
     $c->response->header('Access-Control-Allow-Origin' => '*');
